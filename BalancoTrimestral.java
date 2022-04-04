766c87d628ec04293ecd2714f794d070379b2e1c
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-04-04 16:43:52 +0000

    BalancoTrimestral - Exercicio 1

---
+++ BalancoTrimestral.java
@@ -0,0 +1,11 @@
+public class BalancoTrimestral{
+    public static void main(String[] args) {
+        
+        int gastosAbril = 1212;
+
+        System.out.println(gastosAbril * 10);
+
+    
+
+    }
+}
